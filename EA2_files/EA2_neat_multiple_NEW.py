9e8d4fec466f99cc570f01f5a6c6ff32e3e23508
rickgeling <H.B.R.Geling@student.rug.nl>
2024-10-15 18:57:54 +0200

    changed multiple riun

---
+++ EA2_files/EA2_neat_multiple_NEW.py
@@ -0,0 +1,62 @@
+###############
+# @matushalak
+# code adapted from NEAT-python package, XOr & OpenAI-Lander examples
+# eg. https://github.com/CodeReclaimers/neat-python/blob/master/examples/xor/evolve-feedforward-parallel.py
+###############
+import multiprocessing
+import os
+import neat
+from evoman.environment import Environment
+#from demo_controller import player_controller
+from neat_controller import neat_controller
+from time import time
+from pandas import DataFrame
+import argparse
+import pickle as pkl
+
+from NEAT_EA2_class import ClassNEAT
+
+def parse_args():
+    '''' Function enabling command-line arguments'''
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Optimize weights of Controller NN using EA")
+
+    # Define arguments
+    parser.add_argument('-name', '--exp_name', type=str, required=False, help="Experiment name")
+    parser.add_argument('-mg', '--maxgen', type=int, required=False, default = 10, help="Max generations (eg. 500)")
+    parser.add_argument('-nmes', '--enemies', nargs='+', type=int, required=False, default=[5, 6], help='Provide list(s) of enemies to train against')
+    parser.add_argument('-mult', '--multi', type=str, required=False, default = 'yes', help="Single or Multienemy")
+        #EA1_line_plot_runs   
+    parser.add_argument('-dir', '--directory', type=str, default='1510_EA2_neat_line_plot_runs', required=False, help="Directory to save runs")
+    parser.add_argument('-nruns', '--num_runs', type=int, required=False, default=10, help="Number of repetitive neat runs")  # Unchanged
+    
+    return parser.parse_args()
+
+global cfg, name, enemy_set, base_dir, num_runs, args
+args = parse_args()
+cfg = 'neat_config.txt'
+enemy_set = args.enemies
+base_dir = args.directory
+num_runs = args.num_runs
+
+
+def multiple_runs():
+    # Create the base directory if it does not exist
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+
+    enemy_dir = os.path.join(base_dir, f'EN{enemy_set}')
+    if not os.path.exists(enemy_dir):
+        os.makedirs(enemy_dir)
+
+    for run_i in range(1, num_runs+1):  # Run the EA e.g. 10 times for each enemy
+        run_dir = os.path.join(enemy_dir, f'run_{run_i}_EN{enemy_set}')
+        # Run the ClassicEA for this enemy and run number
+        print(f'\nRunning EA2 (neat) for enemy (set) {enemy_set}, run {run_i}\n')
+        Neat = ClassNEAT(args, run_dir, cfg)
+        Neat.run()
+
+
+
+if __name__ == '__main__':
+    multiple_runs()    
---
+++ EA2_files/NEAT_EA2_class.py
@@ -0,0 +1,99 @@
+###############
+# @matushalak
+# code adapted from NEAT-python package, XOr & OpenAI-Lander examples
+# eg. https://github.com/CodeReclaimers/neat-python/blob/master/examples/xor/evolve-feedforward-parallel.py
+###############
+import multiprocessing
+import os
+import neat
+from evoman.environment import Environment
+# from demo_controller import player_controller
+from neat_controller import neat_controller
+from time import time
+from pandas import DataFrame
+import argparse
+import pickle as pkl
+
+
+class ClassNEAT:
+    def __init__(self, args, run_dir, cfg):
+        self.cfg = cfg
+        self.maxgen = args.maxgen
+        self.enemies = args.enemies
+        self.multi  = 'yes' if args.multi == 'yes' else 'no'
+
+        # add enemy names
+        self.name = run_dir
+        if not os.path.exists(self.name):
+            os.makedirs(self.name)
+
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+    def save_stats(self, stats, filename):
+        '''
+        Saves the evolutionary statistics to a CSV file.
+        '''
+        results = DataFrame({
+            'gen': list(range(len(stats.most_fit_genomes))),
+            'best': [c.fitness for c in stats.most_fit_genomes],
+            'mean': stats.get_fitness_mean(),
+            'sd': stats.get_fitness_stdev()
+        })
+        results.to_csv(filename, index=False)
+
+    def eval_genome(self, genome,config):
+        '''
+        Parallelized version
+        '''
+        env = Environment(experiment_name=self.name,
+                    enemies=self.enemies,
+                    multiplemode=self.multi, 
+                    playermode="ai",
+                    player_controller=neat_controller(config_path=self.cfg), # you  can insert your own controller here
+                    enemymode="static",
+                    level=2,
+                    speed="fastest",
+                    visuals=False)
+
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        fitness ,p,e,t = env.play(pcont=net)
+        return float(p - e) # min player life - max enemy life : modified Gain measure
+        # to focus on beating enemies
+        # return float(100 - e)
+        # return 0.9*(100 - e) + 0.1*(100 - p)
+
+    def run(self):
+        start = time()
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, self.cfg)
+
+        # Initial population
+        pop = neat.Population(config)
+
+        # Add a stdout reporter to show progress in the terminal.
+        pop.add_reporter(neat.StdOutReporter(True))
+        stats = neat.StatisticsReporter()
+        pop.add_reporter(stats)
+
+        # Parallel evaluator
+        parallel_evals = neat.ParallelEvaluator(multiprocessing.cpu_count(), self.eval_genome)
+
+        # Run for N generations
+        pop.run(parallel_evals.evaluate, self.maxgen)
+
+        # winner = pop.run(eval_genomes, 50) # classic
+        winn_gene = stats.best_genome()
+        winner_net = neat.nn.FeedForwardNetwork.create(winn_gene, config)
+
+        # save results
+        self.save_stats(stats, self.name + '/results.txt')
+
+        # save controller
+        with open(self.name + '/best.pkl', 'wb') as f:
+            pkl.dump(winner_net, f)
+
+        end = time()-start
+        print(end)
+
+        # Display winning genome
+        print('\nBest genome:\n{!s}'.format(winn_gene))
